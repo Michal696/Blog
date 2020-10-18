029a6dda02eeaf53b97ace86d10196a0499ecc1a
I516366 <michal.sajdik@sap.com>
2020-10-18 11:09:49 +0200

    Changes in Navigation

--- client/src/components/navbar.component.js
+++ client/src/components/navbar.component.js
@@ -9,15 +9,21 @@ export default class Navbar extends Component {
             <nav className="navbar navbar-dark navbar-expand-lg" style={{backgroundColor: '#111E6C' }}>
                 <div className="navbar-collapse">
                     <ul className="navbar-nav mr-auto">
+                        {/*<li className="navbar-item">*/}
+                        {/*    <Link to="/" className="nav-link">Exercises</Link>*/}
+                        {/*</li>*/}
                         <li className="navbar-item">
-                            <Link to="/" className="nav-link">Exercises</Link>
+                            <Link to="/" className="nav-link">About me</Link>
                         </li>
                         <li className="navbar-item">
-                            <Link to="/api/create" className="nav-link">Create Exercise log</Link>
+                            <Link to="/" className="nav-link">Prolog</Link>
                         </li>
-                        <li className="navbar-item">
-                            <Link to="/api/user" className="nav-link">Create User</Link>
-                        </li>
+                        {/*<li className="navbar-item">*/}
+                        {/*    <Link to="/api/create" className="nav-link">Create Exercise log</Link>*/}
+                        {/*</li>*/}
+                        {/*<li className="navbar-item">*/}
+                        {/*    <Link to="/api/user" className="nav-link">Create User</Link>*/}
+                        {/*</li>*/}
                         <li className="navbar-item">
                             <Link to="/api/articles" className="nav-link">List of Articles</Link>
                         </li>
